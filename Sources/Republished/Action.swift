e7d02d3b5a8e41a6580decdedb680fdca85f9df0
Pat Brown <mrpatbrown@gmail.com>
2021-12-07 17:21:29 +1100

    wip

---
+++ Sources/Republished/Action.swift
@@ -0,0 +1,38 @@
+import Combine
+import SwiftUI
+
+@propertyWrapper
+public struct Action<ObjectType, Value>: DynamicProperty
+  where ObjectType: ObservableObject
+{
+  @UnobservedEnvironmentObject var root: ObjectType
+  let action: (ObjectType) -> Value
+
+  public init<Input, Output>(
+    _ action: @escaping (ObjectType) -> (Input) -> Output
+  ) where Value == (Input) -> Output {
+    self.action = action
+  }
+
+  public init(
+    _ action: @escaping (ObjectType) -> () -> Void
+  ) where Value == () -> Void {
+    self.action = action
+  }
+
+  public init<Input>(
+    _ action: @escaping (ObjectType) -> (Input) -> Void
+  ) where Value == (Input) -> Void {
+    self.action = action
+  }
+
+  public init<Output>(
+    _ action: @escaping (ObjectType) -> () -> (Output)
+  ) where Value == () -> (Output) {
+    self.action = action
+  }
+
+  public var wrappedValue: Value {
+    action(self.root)
+  }
+}
