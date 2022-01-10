f5597e2a525ad07600e2f2f06efe7fcd0b678b0b
Pat Brown <mrpatbrown@gmail.com>
2022-01-10 17:57:36 +1100

    wip

--- Sources/Republished/Republished.swift
+++ Sources/Republished/Republished.swift
@@ -75,11 +75,10 @@ public struct Republished<Value> {
     typealias Output = Value
     typealias Failure = Never
 
-    var cancellable: AnyCancellable?
     weak var changePublisher: ObservableObjectPublisher?
-//    var republishChanges: (Subject, Value) -> Void
-
-    var republish: ((Value) -> Void)?
+    var cancellable: AnyCancellable?
+    var subscribe: (Value, Subject) -> AnyCancellable?
+    var inheritDependencies: (Value, Subject) -> AnyCancellable?
 
     @Published var currentValue: Value {
       willSet { self.changePublisher?.send() }
@@ -88,48 +87,37 @@ public struct Republished<Value> {
 
     init(_ initialValue: Value, inheritDependencies: Bool) where Value: ObservableObject {
       self._currentValue = .init(wrappedValue: initialValue)
-      self.republish = { [weak self] object in
-        guard let self = self else { return }
-        self.cancellable = nil
-        let changeCancellable = object.subscribe(self)
-        let dependencyCancellable = inheritDependencies ? object.inheritDependencies(from: self) : nil
-        self.cancellable = AnyCancellable {
-          _ = (changeCancellable, dependencyCancellable)
-        }
-      }
+      self.subscribe = { $0.subscribe($1) }
+      self.inheritDependencies = { inheritDependencies ? $0.inheritDependencies(from: $1) : nil }
+      self.republish(initialValue)
     }
 
     init<Wrapped>(_ initialValue: Wrapped?, inheritDependencies: Bool) where Wrapped: ObservableObject,
       Wrapped? == Value
     {
       self._currentValue = .init(wrappedValue: initialValue)
-      self.republish = { [weak self] object in
-        guard let self = self else { return }
-        self.cancellable = nil
-        let changeCancellable = object?.subscribe(self)
-        let dependencyCancellable = inheritDependencies ? object?.inheritDependencies(from: self) : nil
-        self.cancellable = AnyCancellable {
-          _ = (changeCancellable, dependencyCancellable)
-        }
-      }
+      self.subscribe = { $0?.subscribe($1) }
+      self.inheritDependencies = { inheritDependencies ? $0?.inheritDependencies(from: $1) : nil }
+      self.republish(initialValue)
     }
 
     init(_ initialValue: Value, inheritDependencies: Bool) where Value: Collection, Value.Element: ObservableObject {
       self._currentValue = .init(wrappedValue: initialValue)
-      self.republish = { [weak self] objects in
-        guard let self = self else { return }
-        self.cancellable = nil
-        let changeCancellables = objects.map { $0.subscribe(self) }
-        let dependencyCancellables = inheritDependencies ? objects.map { $0.inheritDependencies(from: self) } : nil
-
-        self.cancellable = AnyCancellable {
-          _ = (changeCancellables, dependencyCancellables)
-        }
-      }
+      self.subscribe = { $0.subscribe($1) }
+      self.inheritDependencies = { inheritDependencies ? $0.inheritDependencies(from: $1) : nil }
+      self.republish(initialValue)
     }
 
     func receive<S>(subscriber: S) where S: Subscriber, Never == S.Failure, Output == S.Input {
       self.$currentValue.subscribe(subscriber)
+    }
+
+    func republish(_ value: Value) {
+      // TODO: Double check if this needs to be set to nil before subscribing...
+      self.cancellable = nil
+      let changeCancellable = self.subscribe(value, self)
+      let dependenciesCancellable = self.inheritDependencies(value, self)
+      self.cancellable = AnyCancellable { _ = (changeCancellable, dependenciesCancellable) }
     }
   }
 }
@@ -143,5 +131,17 @@ extension ObservableObject {
     Dependencies.bindInheritance(self) {
       [weak subject] in subject?.changePublisher.flatMap(Dependencies.id)
     }
+  }
+}
+
+extension Collection where Element: ObservableObject {
+  func subscribe<Value>(_ subject: Republished<Value>.Subject) -> AnyCancellable {
+    let cancellables = self.map { $0.subscribe(subject) }
+    return AnyCancellable { _ = cancellables }
+  }
+
+  func inheritDependencies<Value>(from subject: Republished<Value>.Subject) -> AnyCancellable {
+    let cancellables = self.map { $0.inheritDependencies(from: subject) }
+    return AnyCancellable { _ = cancellables }
   }
 }
